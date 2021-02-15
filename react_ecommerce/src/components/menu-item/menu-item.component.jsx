9a9f1b618865e5734ec8d0b4328e0540e94d113b
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-15 22:04:49 +0000

    Create MenuItem component

---
+++ react_ecommerce/src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MenuItem = () => (
+  <div className="content">
+    <h1 className="title">HATS</h1>
+    <h1 className="subtitle">SHOP NOW</h1>
+  </div>
+);
+
+export default MenuItem;
